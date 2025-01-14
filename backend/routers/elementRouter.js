f7052ca8d6e7292feb4ca57a3ca92f367784a089
Mariusz Jacek <marios.yaceg@gmail.com>
2025-01-14 21:03:14 +0100

    Router

---
+++ backend/routers/elementRouter.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const elementController = require("./controllers/elementController");
+
+router.route('/').get(
+    elementController.findAll
+).post(elementController.createElement);
+
+module.exports = router;
